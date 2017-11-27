287029c5eaf5f5a0a36efa78b487e3d829f0dc73
zxx <369579262@qq.com>
2017-11-27 21:32:13 +0800

    LeXue commit

---
+++ LeXueZXX/app/src/main/java/zhaoxixiang/bwie/com/lexuezxx/MainActivity.java
@@ -0,0 +1,13 @@
+package zhaoxixiang.bwie.com.lexuezxx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
